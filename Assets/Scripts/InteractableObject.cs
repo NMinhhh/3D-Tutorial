9de555804b232e6dfcf301c1d3ff7944473bcf37
Nhat Minh <leminh2020ct@gmail.com>
2024-07-02 10:13:21 +0700

    Picking up items

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -5,9 +5,33 @@ using UnityEngine;
 public class InteractableObject : MonoBehaviour
 {
     [SerializeField] private string itemName;
-    
+
+    public bool playerInRange;
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0) && playerInRange & SelectionManager.Instance.onTarget)
+        {
+            Debug.Log("Picked up item");
+            Destroy(gameObject);
+        }
+    }
+
     public string GetItemName()
     {
         return itemName;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            playerInRange = false;
     }
 }
--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -5,8 +5,30 @@ using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
+    #region Singleton
+
+    public static SelectionManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    #endregion
+
     [SerializeField] private GameObject interaction_Info_UI;
+
     Text text;
+    
+    public bool onTarget {  get; private set; }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,18 +43,22 @@ public class SelectionManager : MonoBehaviour
         if(Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
-            if (selectionTransform.GetComponent<InteractableObject>())
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+            if (interactable && interactable.playerInRange)
             {
+                onTarget = true;
                 text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else
             {
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
             }
         }
         else
         {
+            onTarget = false;
             interaction_Info_UI.SetActive(false);
         }
     }
